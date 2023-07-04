85c5568e807f3422d9aec7dae3e2a2e3cb6e3c6a
Ayush Kumar Tiwari <91795180+Ayush8085@users.noreply.github.com>
2023-07-04 16:30:57 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -139,6 +139,13 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
