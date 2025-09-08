4e1af031954b9f265608a00c93e5457a7443f649
levijanclavesillas <levijanclavesillas@gmail.com>
2025-09-08 09:45:28 +0800

    Initial commit with .gitignore applied

--- loginsys/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.loginUser, name='login'),
-    path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerUser, name='register'),
-]
